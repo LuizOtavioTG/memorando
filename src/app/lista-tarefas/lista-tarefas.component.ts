75745c169bf5d357860acb3d2e356a6e2a15b98c
LuizOtavioTG <bc.luizo@gmail.com>
2025-10-28 16:09:22 -0300

    Style: Adicionando animação na mensagem de quando não há tarefas.

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { checkButtonTrigger,  highlightedStateTrigger, shownStateTrigger, filterTrigger, formButtonTrigger } from '../animations';
+import { checkButtonTrigger,  highlightedStateTrigger, shownStateTrigger, filterTrigger, formButtonTrigger, flyInOutTrigger } from '../animations';
 
 @Component({
   selector: 'app-lista-tarefas',
@@ -16,6 +16,7 @@ import { checkButtonTrigger,  highlightedStateTrigger, shownStateTrigger, filter
     checkButtonTrigger,
     filterTrigger,
     formButtonTrigger,
+    flyInOutTrigger
   ]
 })
 export class ListaTarefasComponent implements OnInit {
