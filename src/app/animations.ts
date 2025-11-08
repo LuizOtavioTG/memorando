8b9c8f41b154c440e3ab61f23e612cca7d6e7cdc
LuizOtavioTG <bc.luizo@gmail.com>
2025-11-08 17:08:45 -0300

    Style: Adicionando animação de entrada nos cards de tarefas.

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { animate, group, keyframes, query, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, query, stagger, state, style, transition, trigger } from '@angular/animations';
 
 export const highlightedStateTrigger = trigger('highlightedState', [
   state('default', style({
@@ -50,7 +50,7 @@ export const filterTrigger = trigger('filterAnimation', [
     ]))
   ]),
   transition(':leave', [
-    animate('2000ms cubic-bezier(.34,1.94,1,-0.92)', style({ opacity: 0, width: 0 }))
+    animate('2000ms cubic-bezier(.17,.67,.83,.67)', style({ opacity: 0, width: 0 }))
   ])
 ])
 
@@ -122,16 +122,42 @@ export const shakeTrigger = trigger('shakeAnimation', [
   transition('* => *', [
     query('input.ng-invalid:focus, select.ng-invalid:focus', [
       animate('0.5s', keyframes([
-        style({ border: '2px solid red'}),
-        style({ transform: 'translateX(0)'}),
-        style({ transform: 'translateX(-10px)'}),
-        style({ transform: 'translateX(10px)'}),
-        style({ transform: 'translateX(-10px)'}),
-        style({ transform: 'translateX(10px)'}),
-        style({ transform: 'translateX(-10px)'}),
-        style({ transform: 'translateX(10px)'}),
-        style({ transform: 'translateX(0)'}),
+        style({ border: '2px solid red' }),
+        style({ transform: 'translateX(0)' }),
+        style({ transform: 'translateX(-10px)' }),
+        style({ transform: 'translateX(10px)' }),
+        style({ transform: 'translateX(-10px)' }),
+        style({ transform: 'translateX(10px)' }),
+        style({ transform: 'translateX(-10px)' }),
+        style({ transform: 'translateX(10px)' }),
+        style({ transform: 'translateX(0)' }),
       ]))
-    ], {optional: true})
+    ], { optional: true })
+  ])
+])
+
+export const listStateTrigger = trigger('listState', [
+  transition('* => *', [
+    query(':enter', [
+      style({
+        opacity: 0,
+        transform: 'translateX(-100%)'
+      }),
+      stagger(200, [
+        animate('500ms ease-out', keyframes([
+          style({
+            opacity: 1,
+            transform: 'translateX(15%)',
+            offset: 0.4
+          }),
+          style({
+            opacity: 1,
+            transform: 'translateX(0)',
+            offset: 1
+          }),
+        ]))
+      ])
+
+    ], { optional: true })
   ])
 ])
